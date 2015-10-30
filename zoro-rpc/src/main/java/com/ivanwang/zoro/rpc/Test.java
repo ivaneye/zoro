0d901bf664235553b9df7cc6e28e3b06f799b4a7
ivanpig <ivan.pig@foxmail.com>
2015-10-30 11:44:16 +0800

    init

---
+++ zoro-rpc/src/main/java/com/ivanwang/zoro/rpc/Test.java
@@ -0,0 +1,10 @@
+package com.ivanwang.zoro.rpc;
+
+/**
+ * Created by wangyifan on 2015/10/30.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
